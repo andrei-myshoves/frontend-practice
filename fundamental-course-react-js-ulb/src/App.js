bfd45deda74a635b92098e9066a7e138cfa43493
andrei-myshoves <andreimyshoves89@gmail.com>
2025-11-18 22:31:08 +0100

    Second lesson for React

--- fundamental-course-react-js-ulb/src/App.js
+++ fundamental-course-react-js-ulb/src/App.js
@@ -1,23 +1,55 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter"
+import React, { useState, useRef } from "react";
+import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
-import './styles/App.css'
+import "./styles/App.css";
 import PostItem from "./components/PostItem";
-
+import PostList from "./components/Postlist";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
-{id: 1, title:'Javascript', body: 'Description'},
-{id: 2, title:'Javascript 2', body: 'Description'},
-{id: 3, title:'Javascript 3', body: 'Description'},
-	]);
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+  const [selectedSort, setSelectedSort] = useState("");
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    console.log(sort);
+  };
 
   return (
     <div className="App">
-			<h1 style={{textAlign:'center'}}>Список постов</h1>
-			{posts.map((post ) =>
-				<PostItem key={post.id} post={post}/>
-			)}
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }}></hr>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сорртировка"
+          option={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+      {posts.length !== 0 ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>
+      )}
     </div>
   );
 }
