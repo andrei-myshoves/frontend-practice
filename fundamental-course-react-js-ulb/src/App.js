933ca9efcc2ea391b2c287e61bdc84f234f451c9
andrei-myshoves <andreimyshoves89@gmail.com>
2025-11-20 20:53:33 +0100

    lesson four

--- fundamental-course-react-js-ulb/src/App.js
+++ fundamental-course-react-js-ulb/src/App.js
@@ -1,58 +1,17 @@
-import React, { useState, useRef, useMemo } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import React from 'react';
 import "./styles/App.css";
-import PostItem from "./components/PostItem";
-import PostList from "./components/Postlist";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
+import About from './pages/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Posts from './pages/Posts';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "aa", body: "aa" },
-    { id: 2, title: "rr 2", body: "rr" },
-    { id: 3, title: "bb 3", body: "bb" },
-  ]);
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-	const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-  }, [filter.query, sortedPosts]);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-		setModal(false)
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
   return (
-    <div className="App">
-			<MyButton style={{marginTop:'30px'}} onClick={() => setModal(true)}>
-				Создать пользователя
-			</MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: "15px 0" }}></hr>
-      <PostFilter filter={filter} setFilter={setFilter} />
-
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- fundamental-course-react-js-ulb/src/components/Postlist.jsx
+++ fundamental-course-react-js-ulb/src/components/Postlist.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import PostItem from "./PostItem";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const PostList = ({ posts, title, remove }) => {
+	const refs = useRef([]);
   if (!posts.length) {
     return <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>;
   }
@@ -11,11 +12,19 @@ const PostList = ({ posts, title, remove }) => {
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
       <TransitionGroup>
-        {posts.map((post, index) => 
-          <CSSTransition key={post.id} timeout={500} classNames="post">
-            <PostItem remove={remove} number={index + 1}  post={post} />
-          </CSSTransition>
-        )}
+        {posts.map((post, index) => {
+          if (!refs.current[index]) {
+            refs.current[index] = React.createRef();
+          }
+
+          return (
+            <CSSTransition key={post.id} timeout={500} classNames="post" nodeRef={refs.current[index]}>
+              <div ref={refs.current[index]}>
+                <PostItem remove={remove} number={index + 1} post={post} />
+              </div>
+            </CSSTransition>
+          );
+        })}
       </TransitionGroup>
     </div>
   );
