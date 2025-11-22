8906eb7e49609d75543b049adfb11f2e4d097d68
andrei-myshoves <andreimyshoves89@gmail.com>
2025-11-22 17:51:29 +0100

    final lesson

--- fundamental-course-react-js-ulb/src/components/AppRouter.jsx
+++ fundamental-course-react-js-ulb/src/components/AppRouter.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
 import About from "../pages/About";
 import PostIdPage from "../pages/PostIdPage";
+import { publicRoutes, privateRoutes } from "../router";
+import { AuthContext } from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-  return (
+  const {isAuth, isLoading} = useContext(AuthContext);
+	if(isLoading) {
+		return <Loader/>
+	}
+  return isAuth ? (
     <Routes>
-      <Route path="/about" element={<About />} />
-      <Route exact path="/posts/*" element={<Posts />} />
-			<Route exact path="/posts/:id" element={<PostIdPage />} />
-      <Route path="/error" element={<Error />} />
+      {privateRoutes.map((route) => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
 
       {/* Redirect в v6 */}
       <Route path="*" element={<Navigate to="/posts" replace />} />
+    </Routes>
+  ) : (
+    <Routes>
+      {publicRoutes.map((route) => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
+
+      {/* Redirect в v6 */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 };
--- fundamental-course-react-js-ulb/src/pages/Posts.jsx
+++ fundamental-course-react-js-ulb/src/pages/Posts.jsx
@@ -10,6 +10,8 @@ import Loader from "../components/UI/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPageArray, getPageCount } from "../components/utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
+import MySelect from "../components/UI/select/MySelect";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -19,19 +21,23 @@ function Posts() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  
+  const lastElement = useRef();
+  console.log(lastElement);
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers["x-total-count"];
     setTotalPages(getPageCount(totalCount, limit));
   });
 
+  useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1);
+  });
+
   useEffect(() => {
     fetchPosts(limit, page);
-  }, [page]);
+  }, [page, limit]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -42,10 +48,9 @@ function Posts() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
-	const changePage = (page) => {
-		setPage(page)
-		fetchPosts(limit, page)
-	}
+  const changePage = (page) => {
+    setPage(page);
+  };
 
   return (
     <div className="App">
@@ -57,15 +62,25 @@ function Posts() {
       </MyModal>
       <hr style={{ margin: "15px 0" }}></hr>
       <PostFilter filter={filter} setFilter={setFilter} />
+      <MySelect
+        value={limit}
+        onChange={(value) => setLimit(value)}
+        defaultValue="Кол-во элментов на странице"
+        option={[
+          { value: 5, name: "5" },
+          { value: 10, name: "10" },
+          { value: -1, name: "Показать все" },
+        ]}
+      />
       {postError && <h1>Произошла ошибка ${postError}</h1>}
-      {isPostsLoading ? (
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
+      <div ref={lastElement} style={{ height: 20, background: "red" }} />
+      {isPostsLoading && (
         <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
           <Loader />
         </div>
-      ) : (
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
       )}
-    <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
+      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
     </div>
   );
 }
